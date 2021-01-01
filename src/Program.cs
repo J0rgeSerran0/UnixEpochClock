a43781593bcf26ea1c80c817ac74dbda6b2f2d96
Jorge <jorge@MacBook-Pro-de-Jorge.local>
2021-01-01 08:41:01 +0100

    Unix Epoch Clock

--- src/Program.cs
+++ src/Program.cs
@@ -5,7 +5,16 @@ public class UnixEpochClock
     public static void Main(string[] args)
     {
         var date = new DateTime(2021, 1, 1, 2, 7, 2, 285);
+        ShowUnix(date);
+    }
+
+    public static void ShowUnix(DateTime date)
+    {
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.Cyan;
         Console.WriteLine(date.ToString("r"));
+        Console.WriteLine();
+        Console.ResetColor();
 
         var epochTicks = new TimeSpan(new DateTime(1970, 1, 1).Ticks);
         var unixTicks = new TimeSpan(date.Ticks) - epochTicks;
@@ -13,5 +22,6 @@ public class UnixEpochClock
         Console.WriteLine(unixTimestamp);
         Console.WriteLine("HEX: {0}", unixTimestamp.ToString("X"));
         Console.WriteLine("IN MILLISECONDS: {0}", (Int64)unixTicks.TotalMilliseconds);
+        Console.WriteLine();
     }
 }
